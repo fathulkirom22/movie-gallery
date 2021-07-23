cc6b496f07faec827cf03465229924d48c6914fd
@kirom222 <kirom222@gmail.com>
2021-07-23 08:48:14 +0700

    fix main task

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,7 @@
       .title Trending Movies
       div
         no-ssr
-          b-datepicker(v-model="date" range)
-    div {{date}}
+          b-datepicker(v-model="date" placeholder="Filter date" range)
     .d-flex.flex-wrap.justify-content-between
       MovieThumbnail.mb-4(v-for="movie in filtered_movies" :key="movie.id" :movie="movie")
 </template>
@@ -16,32 +15,65 @@ export default {
     this.$axios.get("movies")
       .then(res=>{
         this.movies = res.data
-        this.filtered_movies = res.data
+        this.filterMovies()
       })
   },
-  data:()=>({
-    date: [null, null],
-    movies: [],
-    filtered_movies: [],
-  }),
+  data:()=>({}),
   computed: {
+    movies:{
+      get(){
+        return this.$store.getters['filter/GET_MOVIES']
+      },
+      set(val){
+        this.$store.dispatch('filter/SET_MOVIES', val)
+      }
+    },
+    filtered_movies:{
+      get(){
+        return this.$store.getters['filter/GET_FILTERED_MOVIES']
+      },
+      set(val){
+        this.$store.dispatch('filter/SET_FILTERED_MOVIES', val)
+      }
+    },
+    date:{
+      get(){
+        return this.$store.getters['filter/GET_DATE']
+      },
+      set(val){
+        this.$store.dispatch('filter/SET_DATE', val)
+      }
+    },
     search(){
       return this.$store.getters['filter/GET_SEARCH']
     },
   },
   watch:{
     search(val){
+      console.log('change filter search')
       this.filterMovies()
     },
     date(val){
+      console.log('change filter date')
       this.filterMovies()
     }
   },
   methods:{
     filterMovies(){
-      this.filtered_movies = _.filter(this.movies, (i)=>{
+      let filter_helper;
+      filter_helper = this.filterSearchMovies(this.movies)
+      filter_helper = this.filterDateMovies(filter_helper)
+      this.filtered_movies = filter_helper
+    },
+    filterSearchMovies(movies){
+      return _.filter(movies, (i)=>{
         let re = new RegExp(this.search,"i");
-        return this.date[0] ? i.title.match(re) && new Date(i.showTime) >= new Date(this.date[0]) && new Date(i.showTime) <= new Date(this.date[1]) : i.title.match(re)
+        return this.search ? i.title.match(re) : movies
+      });
+    },
+    filterDateMovies(movies){
+      return _.filter(movies, (i)=>{
+        return this.date[0] && this.date[1] ? new Date(i.showTime) >= new Date(this.date[0]) && new Date(i.showTime) <= new Date(this.date[1]) : movies
       });
     }
   }
--- store/filter.js
+++ store/filter.js
@@ -1,19 +1,34 @@
 export const state = () => ({
+  movies : [],
+  filtered_movies : [],
+  date : [null, null],
   search : null,
 })
 
 export const mutations = {
+  CHANGE_MOVIES(state, val){state.movies = val},
+  CHANGE_FILTERED_MOVIES(state, val){state.filtered_movies = val},
+  CHANGE_DATE(state, val){state.date = val},
   CHANGE_SEARCH(state, val){state.search = val},
 }
 
 export const actions = {
+  SET_MOVIES({commit}, val){commit('CHANGE_MOVIES', val)},
+  SET_FILTERED_MOVIES({commit}, val){commit('CHANGE_FILTERED_MOVIES', val)},
+  SET_DATE({commit}, val){commit('CHANGE_DATE', val)},
   SET_SEARCH({commit}, val){commit('CHANGE_SEARCH', val)},
 
   SET_DEFAULT({commit}){
+    commit('CHANGE_MOVIES', [])
+    commit('CHANGE_FILTERED_MOVIES', [])
+    commit('CHANGE_DATE', [null, null])
     commit('CHANGE_SEARCH', null)
   }
 }
 
 export const getters = {
+  GET_MOVIES:(state)=>(state.movies),
+  GET_FILTERED_MOVIES:(state)=>(state.filtered_movies),
+  GET_DATE:(state)=>(state.date),
   GET_SEARCH:(state)=>(state.search),
 }
